d7bbc3c1a9cfff350c3a4f69c8faf62ef0feccba
YangJiLei <2031291160@qq.com>
2023-08-27 17:00:52 +0800

    chat 初试页面

--- src/views/cserve/Chat.vue
+++ src/views/cserve/Chat.vue
@@ -2,11 +2,20 @@
   <div>
     <el-container>
       <el-aside>
-        <el-input v-model="input" placeholder="请输入内容"></el-input>
-        <ul class="infinite-list listc" v-infinite-scroll="load" style=""  >
-          <li v-for="i in count" class="infinite-list-item">{{i }}</li>
-        </ul>
+        <div>
+          <el-input v-model="input" placeholder="请输入内容" class="ipt" suffix-icon="el-icon-search"></el-input>
+          <div class="did" style="border:3px solid #000">
+            <div v-for="item in users">
+              <div class="listc">{{item.user}}</div>
+            </div>
+          </div>
+        </div>
       </el-aside>
+      <el-main style="background-color: #009cf9">
+        <div>
+
+        </div>
+      </el-main>
     </el-container>
   </div>
 </template>
@@ -15,40 +24,105 @@
 <script>
 export default {
   name: "Chat",
-  data(){
-    return{
-      count: 20,
-      input:"",
+  data() {
+    return {
+      count: 50,
+      input: "",
+      users: [],
     }
 
   },
-  methods:{
-    load(){
+  methods: {
+    load() {
       this.count += 2
     }
   },
   created() {
-    // this.load()
+    this.load()
+    this.users = [
+      {
+        user: '1',
+      },
+      {
+        user: '2',
+      },
+      {
+        user: '3',
+      },
+      {
+        user: '4',
+      },
+      {
+        user: '5',
+      }, {
+        user: '6',
+      },
+      {
+        user: '7',
+      },
+      {
+        user: '8',
+      },
+      {
+        user: '9',
+      },
+      {
+        user: '10',
+      },
+      {
+        user: '11',
+      },
+      {
+        user: '12',
+      },
+      {
+        user: '13',
+      },
+      {
+        user: '14',
+      },
+
+    ]
   }
 }
 </script>
 
 <style scoped>
-[v-cloak]{
+[v-cloak] {
   display: none;
 }
 
-.listc{
-  overflow:auto;
-  height:calc(50vh - 2px);
-  width: 250px;
-  list-style: none;
+.listc {
+  padding-left: 0px;
+  margin-bottom: 0px;
+  margin-top: 0px;
+  height: 70px;
+  border-bottom: 1px solid #000;
 }
-.lic{
-  float:left
-}
+
 
 ::-webkit-scrollbar {
   width: 0px;
+}
+
+.ipt {
+  width: 200px;
+  height: 40px;
+}
+
+.lluser {
+  background-color: #909399;
+  height: 70px;
+  margin-top: 0px;
+}
+
+::v-deep .el-input--mini .el-input__inner {
+  height: 40px;
+}
+
+.did {
+  margin-top: 10px;
+  height: 75vh;
+  overflow: scroll;
 }
 </style>
